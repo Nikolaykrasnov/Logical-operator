1885cceb823ecfdbfacace3efd0e2b6a2928387a
Nikolaykrasnov <crasnov.colya2000@yandex.ru>
2023-02-23 15:59:34 +0500

    Добавьте файлы проекта.

---
+++ Logical operator.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int pin1, pin2;
+	printf("pin1=");
+	scanf_s("%i",&pin1);
+	printf("pin2=");
+	scanf_s("%i",&pin2);
+
+	if ((pin1 == 123) && (pin2 == 321))
+	{
+		printf("OK");
+	}
+	else
+	{
+		printf("Error");
+	}
+}
